afbb6551bc9baa6db516d90699f9859c64d2ec57
Monster Techno <officialworksuman@gmail.com>
2025-09-01 12:53:10 +0530

    Project Initialize

---
+++ Webview/WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewApp.swift
+//  Webview
+//
+//  Created by Suman Dey on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
